9dc5a15700be803e7823d2e23fa480adb1526db8
yul-codes <137112656+yul-codes@users.noreply.github.com>
2024-03-26 11:49:06 +0300

    update mgp_streamlit_app.py

--- mgp_streamlit_app.py
+++ mgp_streamlit_app.py
@@ -4,7 +4,16 @@ import pandas as pd
 TRAIN = "https://www.dropbox.com/scl/fi/5zy935lqpaqr9lat76ung/music_genre_train.csv?rlkey=ccovu9ml8pfi9whk1ba26zdda&dl=1"
 TEST = "https://www.dropbox.com/scl/fi/o6mvsowpp9r3k2lejuegt/music_genre_test.csv?rlkey=ac14ydue0rzlh880jwj3ebum4&dl=1"
 
-train = pd.read_csv(TRAIN)
-test = pd.read_csv(TEST)
 
-st.write(train.head(10))
+def main():
+    train = pd.read_csv(TRAIN)
+    test = pd.read_csv(TEST)
+
+    st.header('Music Genre Prediction')
+
+    st.dataFrame(train)
+    st.dataFrame(test)
+
+
+if __name__ == "__main__":
+    main()
